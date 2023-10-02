69ed369366e1bbfff5a976edbf1132836d8e8227
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-10-02 09:18:37 +0300

    fix

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PhoneBook from 'components/PhoneBook/PhoneBook';
-import { selectIsLoggedIn } from 'redux/auth/selectors';
-import { useSelector } from 'react-redux';
 
 const Contacts = () => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  return <div>{isLoggedIn && <PhoneBook />}</div>;
+  return (
+    <div>
+      <PhoneBook />
+    </div>
+  );
 };
 
 export default Contacts;
